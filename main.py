3921609559a9bad38aef2c221ccffa3aee1d2ba1
Charlie441324219 <dian.li@ttu.edu>
2017-10-20 16:15:48 -0500

    save for today

--- main.py
+++ main.py
@@ -32,3 +32,15 @@ if __name__ == '__main__':
     #This line filter Twitter Streams to capture data by the keywords
     stream.filter(track=['python'], async=True)
 
+tweets_data_path = 'E:\STADY\expasome\realTimeTweetsFilter\fetched_tweets.txt'
+
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+for line in tweets_file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
+
+print(len(tweets_data))
