7dd059fac4404e93a581d9726d3005f402695408
Charlie441324219 <dian.li@ttu.edu>
2017-10-20 11:49:50 -0500

    real time filter implemented

---
+++ main.py
@@ -0,0 +1,31 @@
+import tweepy
+import time
+import config
+import json
+
+
+# Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords
+    stream.filter(track=['python'])
