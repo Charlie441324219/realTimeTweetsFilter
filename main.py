54d2578d93fe5717a4979eedbbc1de98c1728fd0
Charlie441324219 <dian.li@ttu.edu>
2017-11-19 22:18:50 -0600

    add a loction filter

--- main.py
+++ main.py
@@ -28,18 +28,26 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
 
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(config.consumer_key, config.consumer_secret)
-    auth.set_access_token(config.access_token, config.access_secret)
-    stream = Stream(auth, l)
+    # #This handles Twitter authetification and the connection to Twitter Streaming API
+    # l = StdOutListener()
+    # auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+    # auth.set_access_token(config.access_token, config.access_secret)
+    # stream = Stream(auth, l)
+    #
+    # # keywords for filter
+    # keywords = ['cholesterol', 'EKG', 'Aneurys' ,'Angina', 'Angiogenesis' ,'Coronary Arteries', 'Coronary' , 'LDL' , 'HDL' , 'bypass surgery' , 'steats' ,'high sugar level',
+    #                      'chest pain', 'chest pressure', 'difficulty breathing', 'heart attack', 'blood pressure', 'cardiac arrest',
+    #                      'Shooting left arm pain', 'arm pain', 'shooting pain', 'left arm tingling', 'shortness of breath']
+    #
+    # # Bounding boxes:
+    # northeast = [-78.44,40.88,-66.97,47.64]
+    # texas = [-107.31,25.68,-93.25,36.7]
+    # california = [-124.63,32.44,-113.47,42.2]
+    #
+    # #This line filter Twitter Streams to capture data by the keywords
+    # stream.filter(track = keywords,locations = texas, async=True)
 
-    #This line filter Twitter Streams to capture data by the keywords
-    stream.filter(track=['cholesterol', 'EKG', 'Aneurys' ,'Angina', 'Angiogenesis' ,'Coronary Arteries', 'Coronary' , 'LDL' , 'HDL' , 'bypass surgery' , 'steats' ,'high sugar level',
-                         'chest pain', 'chest pressure', 'difficulty breathing', 'heart attack', 'blood pressure', 'cardiac arrest',
-                         'Shooting left arm pain', 'arm pain', 'shooting pain', 'left arm tingling', 'shortness of breath'], async=True)
+    tweets_data_path = 'E:\\STADY\\expasome\\realTimeTweetsFilter\\fetched_tweets.txt'
 
-tweets_data_path = 'E:\\STADY\\expasome\\realTimeTweetsFilter\\fetched_tweets.txt'
-
-# dataAnalysis = DataAnalysis(tweets_data_path)
-# dataAnalysis.readFile()
+    dataAnalysis = DataAnalysis(tweets_data_path)
+    dataAnalysis.readFile()
